518479fbd4219d88f08ab86638c31b9c1e4d7ec3
Carlos Emilio Hurtado Rojas <146590346+Frostbyte29@users.noreply.github.com>
2026-06-10 05:11:28 -0500

    Add Spanish locale to i18n configuration

--- astro.config.ts
+++ astro.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     }),
   ],
   i18n: {
-    locales: ["en"],
+    locales: ["en", "es"],
     defaultLocale: "en",
     routing: {
       prefixDefaultLocale: false,
